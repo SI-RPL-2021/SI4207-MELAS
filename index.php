2a16fb2e343c2effdf45bdffb105eb344befda97
alifyacallistam <alifyacallistam@gmail.com>
2021-04-18 15:26:15 +0700

    menambahkan fitur login ref #1

--- index.php
+++ index.php
@@ -32,6 +32,8 @@ if (isset($_POST['login'])) {
       // login sukses, alihkan ke halaman home
       header("Location: user/home.php");
     }
+  } else {
+    echo "<script>alert('Username tidak dapat ditemukan!');</script>";
   }
 }
 ?>
@@ -44,8 +46,13 @@ if (isset($_POST['login'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+  <link rel="icon" type="image/png" href="./assets/logo.png" />
+
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
+
+
+
   <link rel="stylesheet" href="auth.css">
 </head>
 
@@ -95,12 +102,23 @@ if (isset($_POST['login'])) {
       </div>
 
     </section>
+    <p><a href="#ex1" rel="modal:open">Open Modal</a></p>
+    <div id="ex1" class="modal">
+      <p>Thanks for clicking. That felt good.</p>
+      <a href="#" rel="modal:close">Close</a>
+    </div>
+
     <div class="footer">
       &copy; Melas, 2021.
     </div>
+
   </main>
 
+
+
   <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+
+
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
